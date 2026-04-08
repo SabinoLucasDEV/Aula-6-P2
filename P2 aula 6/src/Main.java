d73f08dbbbd8e50bfed62d009643f98f8066d0a0
Sabino Lucas <sabinolbn1810@gmail.com>
2026-04-08 10:41:58 -0300

    exercicio corrigido

--- P2 aula 6/src/Main.java
+++ P2 aula 6/src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String x = "Sabino";
-        String y = "sabino";
+        Scanner sc =  new Scanner(System.in);
+        int n = 0;
+        System.out.print("digite a primeiro palavra: ");
+        String x = sc.nextLine();
+        System.out.print("digite a segunda palvra: ");
+        String y = sc.nextLine();
 
         if(x.equals(y)){
             System.out.println("nomes iguais");
@@ -11,12 +17,23 @@ public class Main {
         }
 
         if(x.equalsIgnoreCase(y)){
-            System.out.println("nomes iguas");
+            System.out.println("nomes iguas com maisculo e minusculo");
         }
         else {
-            System.out.println("nomes diferentes");
+            System.out.println("nomes diferentes com maiusculo e minusculo");
         }
-        int z = x.length();
-        System.out.println("a primeira palavra tem: " + z + " letras");
+        System.out.print("qual palavra deseja ver a quantidade de caracteres(1/2)?");
+
+       int z = sc.nextInt();
+        if(z == 1){
+             n = x.length();
+        }
+        else if (z == 2) {
+             n = y.length();
+        }
+        else{
+            System.out.println("numero invalido");
+        }
+        System.out.println("a primeira palavra tem: " + n + " letras");
     }
 }
