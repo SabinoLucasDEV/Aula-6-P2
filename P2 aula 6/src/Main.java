5eaff3092d02ee78835e99cba1af633bc558f655
Sabino Lucas <sabinolbn1810@gmail.com>
2026-04-08 11:29:43 -0300

    questão 6 correta

--- P2 aula 6/src/Main.java
+++ P2 aula 6/src/Main.java
@@ -3,14 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
-       String x = "computação";
-       char p1 = x.charAt(0);
-       char p3 = x.charAt(2);
-       char pf = x.charAt(9);
-       System.out.println("primeiro caracter: " + p1 + ", terceiro: "+ p3 + ", ultimo: " + pf);
 
+        System.out.println("Digite a frase: ");
+        String frase = sc.nextLine();
+        System.out.println("digite a palavra de deseja buscar");
+        String bucar = sc.nextLine();
+        int posicao = frase.indexOf(bucar);
 
+        if(posicao != -1) {
+            System.out.println("a palavra aparece na posição: " + posicao);
 
+        }
+        else{
+            System.out.println("a palavra não foi encontrada ");
+        }
 
     }
 }
