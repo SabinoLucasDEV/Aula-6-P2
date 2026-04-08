64808ade3be1af89ed963277adf129c85f14efc3
Sabino Lucas <sabinolbn1810@gmail.com>
2026-04-08 11:17:39 -0300

    questão 4

--- P2 aula 6/src/Main.java
+++ P2 aula 6/src/Main.java
@@ -3,37 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
-        int n = 0;
-        System.out.print("digite a primeiro palavra: ");
-        String x = sc.nextLine();
-        System.out.print("digite a segunda palvra: ");
-        String y = sc.nextLine();
+       String x = "Programação";
+       String parte1 = x.substring(0,8);
+       String parte2 = x.substring(8);
+       System.out.println("primeira parte: " + parte1 +", segunda parte: " + parte2);
 
-        if(x.equals(y)){
-            System.out.println("nomes iguais");
-        }
-        else{
-            System.out.println("nomes diferente");
-        }
 
-        if(x.equalsIgnoreCase(y)){
-            System.out.println("nomes iguas com maisculo e minusculo");
-        }
-        else {
-            System.out.println("nomes diferentes com maiusculo e minusculo");
-        }
-        System.out.print("qual palavra deseja ver a quantidade de caracteres(1/2)?");
 
-       int z = sc.nextInt();
-        if(z == 1){
-             n = x.length();
-        }
-        else if (z == 2) {
-             n = y.length();
-        }
-        else{
-            System.out.println("numero invalido");
-        }
-        System.out.println("a primeira palavra tem: " + n + " letras");
+
     }
 }
